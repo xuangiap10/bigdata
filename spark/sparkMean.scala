32f7d4760425aef555022086537950edd5e02580
Hang <hangmt03bk@yahoo.com>
2023-03-18 11:34:07 -0500

    [Hang] spark project

---
+++ spark/sparkMean.scala
@@ -0,0 +1,53 @@
+/*
+val csv = sc.textFile("mtcars.csv") 
+val headerAndRows = csv.map(line => line.split(",").map(_.trim))
+val header = headerAndRows.first
+val population = headerAndRows.filter(_(0) != header(0)).map(p => (p(2), p(1).toDouble)).cache()
+*/
+
+val csv = sc.textFile("airquality.csv") 
+val headerAndRows = csv.map(line => line.split(",").map(_.trim))
+val header = headerAndRows.first
+val population = headerAndRows.filter(_(0) != header(0)).map(p => (p(5), p(4).toDouble)).cache()
+
+  
+import org.apache.spark.rdd.RDD
+def meanVarFunc(rdd: RDD[(String,Double)]): RDD[(String,(Double,Double))] = {
+	rdd.groupByKey().mapValues(values => {
+	val n = values.size
+	val mean = values.sum / n
+	val variance = values.map(x => math.pow(x - mean, 2)).sum / n
+	(mean, variance)
+	})
+}
+
+//-------------------step 3: Compute the mean and variance for each category 
+println("----population mean and variance-----")
+val meanVarPop = meanVarFunc(population)
+meanVarPop.collect().sorted.foreach(println)
+
+//-------------------step 4: take 25% of the population without replacement
+val sample = population.sample(false,0.25)
+
+//-------------------step 5: do 100 times
+var rddReSample: RDD[(String, (Double, Double))] = sc.emptyRDD[(String, (Double, Double))]
+val count = 1000
+for(i <- 1 to count){
+	//println(i)
+	//---------step 5a: take 100% of the sample with replacement.
+	val resampledData = sample.sample(true,1)
+	//---------step 5b: same as step 3
+	val meanVarResample = meanVarFunc(resampledData)
+	//---------step 5c: Keep adding the values in some running sum
+	rddReSample = rddReSample ++ meanVarResample
+}
+
+//-------------------step 6: get the average and display the result
+val rddSample = rddReSample.mapValues(v => (v._1,v._2,1))
+val rddSampleReduce = rddSample.reduceByKey((v1,v2) => (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3))
+
+val meanVarSample = rddSampleReduce.map{case (k, (m,v,cnt)) => (k,(m/cnt,v/cnt))}
+meanVarSample.collect().sorted.foreach(println)
+
+
+
