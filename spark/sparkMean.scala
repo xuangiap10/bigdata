13d4d8066400fec4d353c7137046b555471f544d
Hang <hangmt03bk@yahoo.com>
2023-03-19 14:49:11 -0500

    [Hang] spark function

--- spark/sparkMean.scala
+++ spark/sparkMean.scala
@@ -1,18 +1,13 @@
+/*
 val filename = "airquality.csv"
 val cateCol = 5
 val numCol = 4
-/*
+
 val filename = "mtcars.csv"
 val cateCol = 2
 val numCol = 1
 */
 
-val csv = sc.textFile(filename) 
-val headerAndRows = csv.map(line => line.split(",").map(_.trim))
-val header = headerAndRows.first
-val population = headerAndRows.filter(_(0) != header(0)).map(p => (p(cateCol), p(numCol).toDouble)).cache()
-
-  
 import org.apache.spark.rdd.RDD
 def meanVarFunc(rdd: RDD[(String,Double)]): RDD[(String,(Double,Double))] = {
 	rdd.groupByKey().mapValues(values => {
@@ -31,29 +26,47 @@ def outputFunc(rdd: RDD[(String,(Double,Double))]): Unit = {
 	rddDS.show()
 }
 
-//-------------------step 3: Compute the mean and variance for each category 
-println("----population mean and variance-----")
-val meanVarPop = meanVarFunc(population)
-outputFunc(meanVarPop)
+def sparkMeanVar(filename: String, cateCol: Int, numCol: Int, fraction: Double = 0.25, count:Int = 1000): Unit = {
+	println("----- filename: " + filename)
+	println("----- cateCol: " + cateCol)
+	println("----- numCol: " + numCol)
+	
+	//-------------------step 2: create a pairRDD called “population”
+	val csv = sc.textFile(filename) 
+	val headerAndRows = csv.map(line => line.split(",").map(_.trim))
+	val header = headerAndRows.first
+	val population = headerAndRows.filter(_(0) != header(0)).map(p => (p(cateCol), p(numCol).toDouble)).cache()
 
-//-------------------step 4: take 25% of the population without replacement
-val sample = population.sample(false,0.25)
+	//-------------------step 3: Compute the mean and variance for each category 
+	println("----population mean and variance-----")
+	val meanVarPop = meanVarFunc(population)
+	outputFunc(meanVarPop)
 
-//-------------------step 5: do 100 times
-var arrSample:Array[(String, (Double, Double, Int))] = Array.empty[(String, (Double, Double, Int))]
-val count = 1000
-for(i <- 1 to count){
-	//println(i)
-	//---------step 5a: take 100% of the sample with replacement.
-	val resampledData = sample.sample(true,1)
-	//---------step 5b: same as step 3
-	val meanVarResample = meanVarFunc(resampledData)
-	//---------step 5c: Keep adding the values in some running sum
-	arrSample = arrSample ++ meanVarResample.mapValues(v => (v._1,v._2,1)).collect()
+	//-------------------step 4: take 25% of the population without replacement
+	println("----sample mean and variance-----")
+	//val fraction = 0.25
+	println("----- fraction: " + fraction)
+	val sample = population.sample(false,fraction)
+
+	//-------------------step 5: do 1000 times
+	var arrSample:Array[(String, (Double, Double, Int))] = Array.empty[(String, (Double, Double, Int))]
+	//val count = 100
+	println("----- count: " + count)
+	for(i <- 1 to count){
+		//println(i)
+		//---------step 5a: take 100% of the sample with replacement.
+		val resampledData = sample.sample(true,1)
+		//---------step 5b: same as step 3
+		val meanVarResample = meanVarFunc(resampledData)
+		//---------step 5c: Keep adding the values in some running sum
+		arrSample = arrSample ++ meanVarResample.mapValues(v => (v._1,v._2,1)).collect()
+	}
+
+	//-------------------step 6: get the average and display the result
+	val rddSampleReduce = sc.parallelize(arrSample).reduceByKey((v1,v2) => (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3))
+	val meanVarSample = rddSampleReduce.map{case (k, (m,v,cnt)) => (k,(m/cnt,v/cnt))}
+	outputFunc(meanVarSample)
 }
 
-//-------------------step 6: get the average and display the result
-val rddSampleReduce = sc.parallelize(arrSample).reduceByKey((v1,v2) => (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3))
-val meanVarSample = rddSampleReduce.map{case (k, (m,v,cnt)) => (k,(m/cnt,v/cnt))}
-outputFunc(meanVarSample)
-exit
+sparkMeanVar("mtcars.csv",2,1)
+//sparkMeanVar("airquality.csv",5,4)
